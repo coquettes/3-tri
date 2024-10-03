837e5c805dd03dc06562245d034f0ea21a476a39
coquettes <161481325+coquettes@users.noreply.github.com>
2024-10-03 12:08:44 -0300

    ppip

--- main.js
+++ main.js
@@ -24,7 +24,7 @@
 let estados= ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
 
 let i=0;
-while(i<'length'){
+while(i<estados.length){
 
 console.log(i);
 console.log(estados[i]);
