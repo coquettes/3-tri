bd8e86deb06427c29496f39ad43bf2e374f28dc7
coquettes <161481325+coquettes@users.noreply.github.com>
2024-10-03 12:15:49 -0300

    xx

--- main.js
+++ main.js
@@ -21,12 +21,16 @@
 // }
 // document.write(areaQuadrado(lado));
 
-let estados= ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SP','SE','TO'];
+let estados = ['AC', 'AL', 'AP', 'AM', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MT', 'MS', 'MG', 'PA', 'PB', 'PR', 'PE', 'PI', 'RJ', 'RN', 'RS', 'RO', 'RR', 'SC', 'SP', 'SE', 'TO'];
 
-let i=0;
-while(i<estados.length){
+let i = 0;
+while (i < estados.length) {
 
-console.log(i);
-console.log(estados[i]);
-i++;
+    console.log(i);
+    console.log(estados[i]);
+    i++;
+}
+for(let j=0;estados.length;j++){
+    console.log(j);
+    console.log(estados[j]);
 }
