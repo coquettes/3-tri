10c16b3a0190fadf510e6eb4072f460fa33d47e0
coquettes <161481325+coquettes@users.noreply.github.com>
2024-10-03 11:22:49 -0300

    xx

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-alert('John e Cecília')
+//alert('John e Cecília')
 
 let num1=parseInt(prompt('qual o numero'));
 let num3=parseInt(prompt('qual o numero'));
@@ -8,11 +8,15 @@ function media(x,y,z){
 }
 document.write(media(num1,num2,num3));
 
+let num1=parseInt(prompt('qual o numero'))
+let num2=parseInt(prompt('qual o numero'))
 function soma(numero1,numero2){
     return numero1+numero2;
 }
+document.write(soma(num1,num2));
+
 console.log(soma(3,5));
 function areaQuadrado(lado){
     return lado**2;
 }
-console.log(areaQuadrado(num));
+
