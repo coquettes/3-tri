a5f82e48f1430261d674117d67923417eca40a5b
coquettes <161481325+coquettes@users.noreply.github.com>
2024-10-03 11:28:44 -0300

    XX

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
-alert('John e Cecília')
+// alert('John e Cecília')
 
-let num1=parseInt(prompt('qual o numero'));
-let num3=parseInt(prompt('qual o numero'));
-let num2=parseInt(prompt('qual o numero'));
-function media(x,y,z){
-    return (x+y+z)/3;
-}
-document.write(media(num1,num2,num3));
+// let num1=parseInt(prompt('qual o numero'));
+// let num3=parseInt(prompt('qual o numero'));
+// let num2=parseInt(prompt('qual o numero'));
+// function media(x,y,z){
+//     return (x+y+z)/3;
+// }
+// document.write(media(num1,num2,num3));
 
-let num1=parseInt(prompt('qual o numero'))
-let num2=parseInt(prompt('qual o numero'))
-function soma(numero1,numero2){
-    return numero1+numero2;
-}
-document.write(soma(num1,num2));
+// let num1=parseInt(prompt('qual o numero'))
+// let num2=parseInt(prompt('qual o numero'))
+// function soma(numero1,numero2){
+//     return numero1+numero2;
+// }
+// document.write(soma(num1,num2));
 
-console.log(soma(3,5));
-function areaQuadrado(lado){
-    return lado**2;
-}
-document.write(areaQuadrado(lado));
+// console.log(soma(3,5));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// document.write(areaQuadrado(lado));
+
+let estados= ['AC','AL','AP','AM','BA','CE','DF','ES','GO','MA','MT','MS','MG','PA','PB','PR','PE','PI','RJ','RN','RS','RO','RR','SC','SE','TO'];
